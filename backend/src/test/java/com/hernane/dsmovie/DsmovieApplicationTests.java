0c82e90531f264a6c6c17948b4c4c7ba39cd95c3
Hernane Faria <fariahernane99@gmail.com>
2022-01-11 18:50:49 -0300

    Project created

---
+++ backend/src/test/java/com/hernane/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hernane.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
